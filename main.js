3a1b88b16f989b900f96808d16d0dd8683363464
Federico <federicogonz2016@gmail.com>
2021-12-25 03:34:32 -0300

    Añadiendo efecto Scroll Reveal con js

--- main.js
+++ main.js
@@ -3,3 +3,8 @@ document.querySelector('.menu-btn').addEventListener('click', () => {
     document.querySelector('.nav-menu').classList.toggle('show')
 
 });
+
+ScrollReveal().reveal('h1' ,{delay: 500});
+ScrollReveal().reveal('.imagen-principal' ,{delay: 700});
+ScrollReveal().reveal('.proyectos' ,{delay: 700});
+ScrollReveal().reveal('footer' ,{delay: 1000});
