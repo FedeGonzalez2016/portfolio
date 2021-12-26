bca4ba62e1caa971cc19cefed83495b368e0c85b
Federico <federicogonz2016@gmail.com>
2021-12-26 00:19:34 -0300

    Creacion de Menu Hamburguesa y retoques para accesibilidad via Smartphone

--- main.js
+++ main.js
@@ -1,8 +1,3 @@
-document.querySelector('.menu-btn').addEventListener('click', () => {
-
-    document.querySelector('.nav-menu').classList.toggle('show')
-
-});
 
 ScrollReveal().reveal('h1' ,{delay: 500});
 ScrollReveal().reveal('.imagen-principal' ,{delay: 700});
