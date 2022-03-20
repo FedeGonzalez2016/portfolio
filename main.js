af22b61be6b3de4d67c8f67dd17885f9be565fc1
Federico <federicogonz2016@gmail.com>
2022-03-20 11:26:10 -0300

    Se quita ScrollReveal

--- main.js
+++ main.js
@@ -1,5 +1,7 @@
-
+/*
 ScrollReveal().reveal('h1' ,{delay: 500});
 ScrollReveal().reveal('.imagen-principal' ,{delay: 700});
 ScrollReveal().reveal('.proyectos' ,{delay: 700});
 ScrollReveal().reveal('footer' ,{delay: 700});
+
+*/
